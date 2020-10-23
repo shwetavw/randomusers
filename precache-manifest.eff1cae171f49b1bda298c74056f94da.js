551077e2888d7b0372560c3b07e599029b70b1f2
shwetavw <shwetavijay369@gmail.com>
2020-10-23 11:13:55 +0800

    Updates

---
+++ precache-manifest.eff1cae171f49b1bda298c74056f94da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5a4b3d97273d40b2ee7d0de93f417f1",
+    "url": "/randomusers/index.html"
+  },
+  {
+    "revision": "ec4b13ef5cea322af6be",
+    "url": "/randomusers/static/css/main.9f19476c.chunk.css"
+  },
+  {
+    "revision": "95f123f7c24d4fbe7edf",
+    "url": "/randomusers/static/js/2.184f0f5b.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/randomusers/static/js/2.184f0f5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec4b13ef5cea322af6be",
+    "url": "/randomusers/static/js/main.7d3bf785.chunk.js"
+  },
+  {
+    "revision": "8f81bdca35cfae046b56",
+    "url": "/randomusers/static/js/runtime-main.034335bc.js"
+  }
+]);
